ed7bdf76c1880c2863c8e3f45773cd0133b74f7b
ArTeGaS-2 <probablytorvik@gmail.com>
2025-10-05 20:03:08 +0300

    Дописали скрипт перемикача сцен, та додали об'єкти

--- Assets/SwitchScene.cs
+++ Assets/SwitchScene.cs
@@ -6,14 +6,24 @@ using UnityEngine.SceneManagement;
 public class SwitchScene : MonoBehaviour
 {
     [Header("Об'єкти гри")]
-    public GameObject GameFolderObj; // Об'єкти гри на сцені
-    public GameObject GameUIFolderObj; // Елементи інтерфейсу гри на сцені
+    public GameObject gameFolderObj; // Об'єкти гри на сцені
+    public GameObject gameUIFolderObj; // Елементи інтерфейсу гри на сцені
+
     [Header("Об'єкти магазину")]
-    public GameObject ShopFolderObj; // Обеєкти магазину на сцені
-    public GameObject ShopUIFolderObj; // Елементи інтерфейсу магазину на сцені
+    public GameObject shopFolderObj; // Обеєкти магазину на сцені
+    public GameObject shopUIFolderObj; // Елементи інтерфейсу магазину на сцені
+
+    [Header("Загальне")]
+    public bool ifGameActive = true; // Індикатор стану сцени
 
     public void Switch_Game_Shop_Button()
     {
+        // Міняємо стан сцени гри
+        gameFolderObj.SetActive(!gameFolderObj.activeSelf);
+        gameUIFolderObj.SetActive(!gameUIFolderObj.activeSelf);
 
+        // Міняємо стан сцени магазину
+        shopFolderObj.SetActive(!shopFolderObj.activeSelf);
+        shopUIFolderObj.SetActive(!shopUIFolderObj.activeSelf);
     }
 }
