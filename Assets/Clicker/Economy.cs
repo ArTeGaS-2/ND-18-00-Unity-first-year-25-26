269cd1264f0225c2938db5725953212c45a358fc
ArTeGaS-2 <probablytorvik@gmail.com>
2025-10-19 20:02:31 +0300

    Кнопка магазину як 3D об'єкт та початок економіки і бонусів

---
+++ Assets/Clicker/Economy.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Economy : MonoBehaviour
+{
+    public static Economy Instance; // Сінглтон
+    public int clickCounter = 0; // Лічильник
+}
