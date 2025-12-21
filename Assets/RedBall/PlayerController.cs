7868c6ae73b433260904614dec15e895686acae4
ArTeGaS-2 <probablytorvik@gmail.com>
2025-12-21 20:04:47 +0200

    Почали RedBall

---
+++ Assets/RedBall/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float jumpForce = 6f;
+    [SerializeField] float groundCheckDistance = 0.6f;
+
+    Rigidbody rb;
+    bool isGrounded;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();   
+    } 
+    void Update()
+    {
+        isGrounded = Physics.Raycast(
+            transform.position,
+            Vector3.down,
+            groundCheckDistance);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Vector3 velocity = rb.velocity;
+            velocity.y = 0f;
+            rb.velocity = velocity;
+            rb.AddForce(Vector3.up * jumpForce,
+                ForceMode.VelocityChange);
+        }
+
+
+    }
+}
