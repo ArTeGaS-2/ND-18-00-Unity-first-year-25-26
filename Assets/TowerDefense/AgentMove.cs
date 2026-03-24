07fb8db9abc1a049c8f6a71539d87d22fdd2250e
ArTeGaS-2 <probablytorvik@gmail.com>
2026-03-24 13:40:30 +0200

    Працювали над мапою, ворогом і навігацією ШІ на мапі та серед перешкод

--- Assets/TowerDefense/AgentMove.cs
+++ Assets/TowerDefense/AgentMove.cs
@@ -10,9 +10,20 @@ public class AgentMove : MonoBehaviour
     [SerializeField] NavMeshAgent agent;
     // Максимальна кількість очок здоров'я агента
     [SerializeField] float maxHitPoints = 20f;
+    // Мінімальна кількість здоров'я
+    [SerializeField] float minHitPoints = 0f;
 
     [Header("Інше")]
     // Ім'я об'єкта, який буде ціллю для агента
     [SerializeField] string targetName = "TargetPoint";
 
+    private void Awake()
+    {
+        // Отримуємо компонент "агент" з ворога
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(           // Встановлення цілі об'єкту
+            GameObject.Find(targetName  // Пошук на сцені за іменем
+            ).transform.position);      // Позиція цілі
+    }
 }
