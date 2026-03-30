e202a63efa22790b368df56d66f4e53b02633d9f
ArTeGaS-2 <probablytorvik@gmail.com>
2026-03-30 16:04:01 +0300

    Додали рух проджектайла до цілі

---
+++ Assets/TowerDefense/Tower/Projectile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject targetObj; // Посилання на ціль на сцені
+    [SerializeField] float projectileSpeed = 1.0f; // швидкість
+
+    private void FixedUpdate()
+    {
+        // Рух до цілі
+        transform.position = Vector3.MoveTowards(
+            transform.position, // Вихідні координати
+            targetObj.transform.position, // цільова позиція
+            projectileSpeed * Time.fixedDeltaTime); // швидкість
+
+        transform.LookAt(targetObj.transform); // обертання
+    }
+}
