36053aba7608f2e0136669e122aaed0996df0fd2
ArTeGaS-2 <probablytorvik@gmail.com>
2026-05-25 14:50:58 +0300

    Додали базову атаку вежі по ворогах

--- Assets/TowerDefense/Tower/Tower.cs
+++ Assets/TowerDefense/Tower/Tower.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [Header("Об'єкти")]
     [SerializeField] GameObject projectile; // Посилання на снаряд
     [SerializeField] GameObject spawnPoint; // точка спавну снаряду
+
+    [Header("Параметри вежі")]
     public float attackInterval = 1f; // Інтервал між пострілами 
-    
+    public float attackDamage = 1f; // Шкода від вежі
+    public float attackRadius = 5f; // Радіус атаки
+
+    private List<GameObject> enemiesList; // Список ворогів, які знаходяться в радіусі атаки
+
+    private bool enemiInRadius; // Чи є вороги в радіусі атаки
+    private Coroutine towerAttack; // Посилання на корутину атаки вежі
+
+    private void Awake()
+    {
+        enemiesList = new List<GameObject>(); // Ініціалізація списка
+    }
+
     private void Start()
     {
-        StartCoroutine(Shoot());
+        // StartCoroutine(Shoot());
+        towerAttack = null;
     }
     private IEnumerator Shoot()
     {
         while (true) 
         {
-            yield return new WaitForSeconds(attackInterval); // Затримку у сек
-            Instantiate(
+            if (enemiesList.Count > 0)
+            {
+                Projectile projectileScript = Instantiate(
                 projectile, // Об'єкт який створюємо
                 spawnPoint.transform.position, // Точка виходу снаряду
-                Quaternion.identity); // Обертання
+                Quaternion.identity).GetComponent<Projectile>();
+
+                projectileScript.targetObj = enemiesList[0];
+            }
+            yield return new WaitForSeconds(attackInterval); // Затримку у сек
+            
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy")) // перевіряємо тег
+        {
+            if (enemiesList.Count == 0) // якщо ворогів ще немає в радіусі
+            {
+                towerAttack = StartCoroutine(Shoot()); // починає стріляти
+            }
+            enemiesList.Add(other.gameObject); // додаємо ворога у список
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy")) // перевіряємо тег
+        {
+            if (enemiesList.Count == 0) // якщо ворогів ще немає в радіусі
+            {
+                StopCoroutine(towerAttack);
+            }
+            enemiesList.Remove(other.gameObject);
         }
     }
 }
